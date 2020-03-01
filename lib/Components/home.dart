22cc0ea18416ac0f4df476bfd53fae120bc5be12
masaki12345 <masasaki3107@gmail.com>
2020-03-01 13:44:47 +0900

    Splitfile

--- lib/Components/home.dart
+++ lib/Components/home.dart
@@ -1,92 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:prototype/Models/col.dart';
-import 'package:prototype/Services/processing.dart';
 
 import 'Add.dart';
+import 'bottom.dart';
+import 'list.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size * 0.78;
+    final Size size = MediaQuery.of(context).size * 0.8;
     final Size heightsize = MediaQuery.of(context).size * 0.2;
     return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 244, 225, 1),
       appBar: AppBar(
         title: Text("Flutter List"),
       ),
-      body: StreamBuilder(
-        stream: Processing().getNotes(),
-        builder: (BuildContext context, AsyncSnapshot<List<Col>> snapshot) {
-          return Column(
-            children: <Widget>[
-              SizedBox(
-                height: size.height,
-                child: ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Col col = snapshot.data[index];
-                      return Container(
-                          margin: EdgeInsets.all(15),
-                          height: heightsize.height,
-                          decoration: BoxDecoration(
-                            color: Colors.redAccent,
-                            border: Border.all(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Text(col.test));
-                    }),
-              ),
-              FloatingActionButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => Add(),
-                  ),
-                ),
-                child: Icon(
-                  Icons.add,
-                  color: Colors.black,
-                ),
-                backgroundColor: Colors.red,
-              ),
-            ],
-          );
-        },
+      body: Container(
+        height: MediaQuery.of(context).size.height - 50.0,
+        width: double.infinity,
+        child: Lists(),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => Add(),
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(230, 122, 122, 1),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomBar(),
     );
   }
 }
-
-// child: ListTile(
-//   title: Row(
-//     children: <Widget>[
-//       Expanded(
-//         child: Text(col.test),
-//       ),
-//     ],
-//   ),
-//   subtitle: Text(col.samople),
-//   trailing: Row(
-//     mainAxisSize: MainAxisSize.min,
-//     children: <Widget>[
-//       IconButton(
-//         icon: Icon(Icons.edit),
-//         color: Colors.blue,
-//         onPressed: () => Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//             builder: (_) => Retouching(
-//               col: col,
-//             ),
-//           ),
-//         ),
-//       ),
-//       IconButton(
-//         icon: Icon(Icons.delete),
-//         color: Colors.red,
-//         onPressed: () =>
-//             Processing().deleteNote(col.id),
-//       ),
-//     ],
-//   ),
-// ),
