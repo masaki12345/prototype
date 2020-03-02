b51b93638686ccf285e1d1af77cbbb781aed2106
masaki12345 <masasaki3107@gmail.com>
2020-03-03 00:22:54 +0900

    dialogss

--- lib/Components/add.dart
+++ lib/Components/add.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:prototype/Models/col.dart';
 import 'package:prototype/Services/processing.dart';
+
+import 'dialog.dart';
 
 class Add extends StatefulWidget {
   final Col col;
@@ -14,7 +17,9 @@ class _AddState extends State<Add> {
   GlobalKey<FormState> _key = GlobalKey<FormState>();
   TextEditingController _testController;
   TextEditingController _samopleController;
+
   String _type = '';
+
 //初期化
   @override
   void initState() {
@@ -42,82 +47,78 @@ class _AddState extends State<Add> {
       appBar: AppBar(
         title: Text('追加'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _key,
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                RadioListTile(
-                  activeColor: Colors.blue,
-                  title: Text('CSS'),
-                  value: "CSS",
-                  groupValue: _type,
-                  onChanged: _handleRadio,
-                ),
-                RadioListTile(
-                  title: Text('JavaScript'),
-                  value: "JavaScript",
-                  groupValue: _type,
-                  onChanged: _handleRadio,
-                ),
-                RadioListTile(
-                  title: Text('HTML'),
-                  value: "HTML",
-                  groupValue: _type,
-                  onChanged: _handleRadio,
-                ),
-                TextFormField(
-                  textInputAction: TextInputAction.next,
-                  controller: _testController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty)
-                      return 'title not empty';
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'title',
-                    border: OutlineInputBorder(),
+      body: StreamBuilder(
+          stream: Processing().getcategory(),
+          builder: (BuildContext context, AsyncSnapshot<List<Col>> snapshot) {
+            Col col = snapshot.data[1];
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _key,
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      RadioListTile(
+                        activeColor: Colors.blue,
+                        title: Text(col.category_color),
+                        value: "CSS",
+                        groupValue: _type,
+                        onChanged: _handleRadio,
+                      ),
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        controller: _testController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty)
+                            return 'title not empty';
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'title',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        textInputAction: TextInputAction.done,
+                        controller: _samopleController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          labelText: 'description',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      RaisedButton(
+                        color: Theme.of(context).accentColor,
+                        textColor: Colors.white,
+                        child: Text('save'),
+                        onPressed: () async {
+                          if (_key.currentState.validate()) {
+                            try {
+                              await Processing().addNote(
+                                Col(
+                                    test: _testController.text,
+                                    samople: _samopleController.text,
+                                    category: _type),
+                              );
+                              print(_testController.text +
+                                  _samopleController.text);
+                              Navigator.pop(context);
+                            } catch (e) {
+                              print(e);
+                            }
+                          }
+                        },
+                      ),
+                      Dialogss(),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 10.0),
-                TextFormField(
-                  textInputAction: TextInputAction.done,
-                  controller: _samopleController,
-                  maxLines: 2,
-                  decoration: InputDecoration(
-                    labelText: 'description',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                RaisedButton(
-                  color: Theme.of(context).accentColor,
-                  textColor: Colors.white,
-                  child: Text('save'),
-                  onPressed: () async {
-                    if (_key.currentState.validate()) {
-                      try {
-                        await Processing().addNote(
-                          Col(
-                              test: _testController.text,
-                              samople: _samopleController.text,
-                              category: _type),
-                        );
-                        Navigator.pop(context);
-                      } catch (e) {
-                        print(e);
-                      }
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+              ),
+            );
+          }),
     );
   }
 }
